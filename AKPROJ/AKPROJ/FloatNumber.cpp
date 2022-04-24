6a3dd4c525ed2387fa06c2d985d43f952c0fc441
iniewczas_252814 <iniewczas_252814@e-science.pl>
2022-04-24 11:54:51 +0200

    Dodanie konwersji dla liczby posiadajaca rownoczesnie czesc calkowita i przecinkowa

--- AKPROJ/AKPROJ/FloatNumber.cpp
+++ AKPROJ/AKPROJ/FloatNumber.cpp
@@ -29,16 +29,11 @@ void FloatNumber::dec2float(float inputNumber)
 
 
 	bool normalized = false;
-
-
-
-
 	int number = inputNumber;
+	float afterTheDecPoint = inputNumber - number;
 	int decPlace = 0;
-
-
 	int tempNumber = number;
-	
+	int fracIterator = 0;				//iterator po mnozniku, jak przechodzi sie z liczby calkowitej do przecinkowej
 	std::cout << "realNumber: "<<std::format("{:b}", number) << std::endl;
 
 
@@ -58,26 +53,16 @@ void FloatNumber::dec2float(float inputNumber)
 
 		//std::cout << std::format("{:b}", number / 2048) << std::endl;
 
-
-
 		if (number / int(pow(2, decPlace)) == 1)
 		{
-			number = number - pow(2, decPlace);
-			
-		}
-
-		
-		
+			number = number - pow(2, decPlace);	
+		}	
 	}
-
-
 
 	for (int i = 0; i < decPlace; i++)
 	{
 		
 		//std::cout << "number: " << number << std::endl;
-
-			
 		//std::cout << std::format("{:b}", byteIterator)<<"\n";
 
 		if(number/int(pow(2, decPlace-i-1))==1)
@@ -96,31 +81,51 @@ void FloatNumber::dec2float(float inputNumber)
 			byte = 0;
 		}
 
-
+		fracIterator++;
 
 	}
 
+	//std::cout << "fracIterator: " << fracIterator << std::endl;
+	//std::cout << "loop: " <<s.getFraction()*8 << std::endl;
+	//std::cout<<std::format("{:b}", byte) << std::endl;
 
-	std::cout<<std::format("{:b}", byte) << std::endl;
+	while ((fracIterator < s.getFraction() * 8)&&afterTheDecPoint!=0)
+	{
+		//std::cout << "i: " << fracIterator << " ";
+		afterTheDecPoint *= 2;
+		if (afterTheDecPoint >= 1)
+		{
+			byte += byteIterator;
+			afterTheDecPoint -= 1;
+		}
 
-	if (byteIterator!=0b10000000)
+		byteIterator >>= 1;
+
+		if (byteIterator == 0)
+		{
+			fracTab.push_back(byte);
+			byte = 0;
+			byteIterator = 0b10000000;
+		}
+
+		fracIterator++;
+	}
+
+	//std::cout << "fracIterator: " << fracIterator << std::endl;
+	//std::cout << "s.getfrac: " << << std::endl;
+	if (fracIterator < s.getFraction()*8)
+	{
+		std::cout << "halo";
 		fracTab.push_back(byte);
+		byte = 0;
+	}
+	while (fracTab.size() != s.getFraction())
+	{
+		fracTab.push_back(0);
+	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
 
 
 
--- AKPROJ/AKPROJ/main.cpp
+++ AKPROJ/AKPROJ/main.cpp
@@ -8,7 +8,7 @@ int main()
 	Standard s(3, 4);
 	FloatNumber number;
 	number.setStandard(s);
-	number.dec2float(0b11100011010111);
+	number.dec2float(14551.75);
 
 
 	
